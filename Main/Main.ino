8de0d2cbbe626d6e364ffc2486241b02ccf8a899
Jack Schumacher <jackrschumacher@gmail.com>
2025-10-17 02:43:28 -0500

    Setup sensors and start ros command processing, fix docs

--- Main/Main.ino
+++ Main/Main.ino
@@ -8,7 +8,7 @@
 #include <Adafruit_BNO055.h>
 #include "SparkFun_TB6612.h"
 #include <utility/imumaths.h>
-#include "PWMDcMotor.hpp"  
+#include "PWMDcMotor.hpp"
 #include <Servo.h>
 // Would reccomend setting up all the global variables here
 // Initialize bno
@@ -55,7 +55,7 @@ Axis 4- End Effector- 45 kg servo
 */
 Servo axis0Servo;
 Servo axis1Servo;
-Servo axis2Servi;
+Servo axis2Servo;
 Servo axis3Servo;
 Servo axis4Servo;
 
@@ -67,16 +67,61 @@ String concatenatedString;
 
 void setup() {
   // Setup serial baud rate at 9600
-  Serial.begin(9600)
+  Serial.begin(9600);
   // Not really nessecerray except for debugging
   pinMode(LED_BUILTIN, OUTPUT);
   // TODO: Determine pins- other values are min and max (also look into)
-  axis0Servo.attach(0,700,2000)
-  axis1Servo.attach(1,700,2000)
-  axis2Servo.attach(2,700,2000)
-  axis3Servo.attach(3,700,2000)
+  axis0Servo.attach(0, 700, 2000);
+  axis1Servo.attach(1, 700, 2000);
+  axis2Servo.attach(2, 700, 2000);
+  axis3Servo.attach(3, 700, 2000);
 
-  pinMode(40, OUTPUT); // Output to Libre
-}
-void loop() {
-}
+  pinMode(40, OUTPUT);  // Output to Libre
+
+  pinMode(PWMA, OUTPUT);
+  pinMode(bin1, OUTPUT);
+  pinMode(bin2, OUTPUT);
+
+  const int sdOutputPin = 9;
+  // Check sensor communications
+  if (!SD.begin(sdOutputPin)) {
+    Serial.println("SD Card Initialization failed. Check card formatting and wiring.");
+  } else {
+    Serial.println("SD Card initilization succeded");
+    if (!bmp.begin_I2C()) {
+      Serial.println("No BMP388 sensor is detected. Please check wiring, pin assignment in both hardware and software,etc. ");
+    }
+    if (!bno.begin()) {
+      Serial.println("No BNO055 detected. Check wiring and pin assignment.")
+    }
+
+    // Set up sensor sampling rates
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  }
+  void loop() {
+    // Check the current time 
+    unsigned long currentIMUTime = millis();
+    // Check if the serial is avaliable
+    if(Serial.avaliable() >0){
+      // Take the serial output on the current line
+      String command = Serial.readStringUntil('\n')
+      command.trim()
+
+      // Add command buffer to an array to allow for easier processing
+      char commandBuffer[command.length() +1]
+      command.toCharArray(commandBuffer,command.length() +1)
+
+      char token =""
+      char delimiter ="a1:"
+      char token = strtok(commandBuffer, delimiter)
+      int axis1Move = atoi(token)
+
+      delimiter = ",ax2:"
+      token = strtok(NULL,delimiter)
+      int axis2Move = atoi(token)
+
+    }
+  }
--- README.md
+++ README.md
@@ -8,6 +8,15 @@ A repository of optimized 2 month code
 
 Pin assignments can be easily updated to suit different hardware configurations.
 
-### Pico
+| **Pin** | Function |
+| ------- | -------- |
+|         |          |
+|         |          |
+|         |          |
+|         |          |
+|         |          |
+|         |          |
+|         |          |
+|         |          |
+|         |          |
 
-<img src="C:\Users\jackr\AppData\Roaming\Typora\typora-user-images\image-20251017003124011.png" alt="image-20251017003124011" style="zoom: 200%;" />
