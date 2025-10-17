56abf71dcac6632939c838d950e3e92178fa3d5b
Jack Schumacher <jackrschumacher@gmail.com>
2025-10-16 20:17:41 -0500

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+#include <Adafruit_BNO055.h>
+#include "SparkFun_TB6612.h"
+#include <utility/imumaths.h>
+#include "PWMDcMotor.hpp" //Determine if this is being used
+#include <Servo.h>
